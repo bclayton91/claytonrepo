033b8140ae14b4f16decdb02a8897b08caa83c09
bclayton7043 <brandonclayton1991gmail.com>
2019-12-21 05:33:43 -0500

    updated shamlink

--- Python Programs/Shamlink.py
+++ Python Programs/Shamlink.py
@@ -1,11 +1,11 @@
-vrf = "ME11"
-routetarget = "4769:5754002"                                  ##Route-target import on the configured router
-loopbk = "loopback911"                                        ##Sham-link loopback interface
-source = "FC00:10:125:94::21"                                 ##Loopback ipv6 address
+vrf = "TENANT1"
+routetarget = "4769:2457002"                                  ##Route-target import on the configured router
+loopbk = "loopback901"                                        ##Sham-link loopback interface
+source = "FC00:10:135:97::21"                                 ##Loopback ipv6 address
 destination = "FC00:10:125:94::21"                            ##Loopback ipv6 address on destination router
 network = (source) + "/128"                                  
 cost = "10"                                                   
-procid = "1111"                                               ##Ospfv3 process id
+procid = "1"                                               ##Ospfv3 process id
 
 
 
