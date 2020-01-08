97b25224660ebabf401a2237db714d76a009c0d0
bclayton7043 <brandonclayton1991gmail.com>
2020-01-08 02:44:12 -0500

    updating netconf filter2

--- Python Programs/netconf_filter.py
+++ Python Programs/netconf_filter.py
@@ -1,6 +1,10 @@
+#### import the manager function from the ncclient extenstion ###
 from ncclient import manager 
+
+### create a dictionary of the router you are going to be connecting to ###
 router = {"host": "192.168.3.1","port":"22","username":"bclayton7043", "password":"temporary1"}
 
+### create a netconf filter to filter the information you want to receive ###
 netconf_filter="""
     <filter>
         <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
@@ -16,7 +20,11 @@ netconf_filter="""
     </filter>
     """
 
-with manager.connect(host=router["host"],port=router["port"],username=router["username"],password=router["password"]) as m:
+### using the manager function, connect to the router using the router information ###
+with manager.connect(host=router["host"],port=router["port"],username=router["username"],
+password=router["password"]) as m:
+
+### use the below commands to print out the yang models the router is capable of using ###
     for capability in m.server_capabilities:
         print ("*"*50)
         print(capability)
