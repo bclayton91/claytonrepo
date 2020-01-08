a7ae3888867019e520948e16b3da5dd2820cdbea
bclayton7043 <brandonclayton1991gmail.com>
2020-01-08 02:57:08 -0500

    netconf

--- Python Programs/netconf_filter.py
+++ Python Programs/netconf_filter.py
@@ -1,6 +1,9 @@
 #### import the manager function from the ncclient extenstion ###
 from ncclient import manager 
 
+### import xml dom.minidom ###
+import xml.dom.minidom
+
 ### create a dictionary of the router you are going to be connecting to ###
 router = {"host": "192.168.3.1","port":"22","username":"bclayton7043", "password":"temporary1"}
 
@@ -28,4 +31,10 @@ password=router["password"]) as m:
     for capability in m.server_capabilities:
         print ("*"*50)
         print(capability)
+### use the manager connection and use the get command to get operational state/config data ###
+        interface_netconf=m.get(netconf_filter)
+        xmlDom = xml.dom.minidom.parseString(str(interface_netconf))
+        print (xmlDom.toprettyxml(indent=""))
+        print ("*"*25+"Break"+"*"*50)
+
     m.close_session()
