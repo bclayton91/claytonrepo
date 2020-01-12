592b3cd2b0f7e3b05326715c7ca8d734f8276612
bclayton7043 <brandonclayton1991gmail.com>
2020-01-12 03:01:26 -0500

    "adding router config"

--- yaml docs/importyaml.py
+++ yaml docs/importyaml.py
@@ -3,5 +3,4 @@ from yaml import load, load_all
 stream = open("people.yaml","r")
 documents = load_all(stream, Loader=yaml.FullLoader)
 print (type(documents))
-for doc in documents:
-    print (type (doc["people"][0]))
+
