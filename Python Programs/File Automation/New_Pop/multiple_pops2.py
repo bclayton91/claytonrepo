72bc9022cd41366ed4918d80ae127e1c62eec102
bclayton7043 <brandonclayton1991gmail.com>
2020-12-07 17:47:43 -0500

    updated pop configuration program

--- Python Programs/File Automation/New_Pop/multiple_pops2.py
+++ Python Programs/File Automation/New_Pop/multiple_pops2.py
@@ -11,52 +11,73 @@ env = Environment(loader=file_loader)
 
 # Create configuration generator functions
 def get_xper_config():
-    output = template.render(
-    third_oct=site["third_octet"],
-    Gait_ip=site["Gait_ip"],
-    Disa_ip=site["Disa_ip"],
-    CCSD=site["ccsd"],
-    csc=site["csc_subnet"],
-    hostname=site["hostname"],
-    username=site["username"],
-    password=site["password"])
+    if site["get_xper_config"] == "True":
+            output = template.render(
+            third_oct=site["third_octet"],
+            Gait_ip=site["Gait_ip"],
+            Disa_ip=site["Disa_ip"],
+            CCSD=site["ccsd"],
+            csc=site["csc_subnet"],
+            hostname=site["hostname"],
+            username=site["username"],
+            password=site["password"])
 
-    doc_name = f'{site["hostname"]} xper'
-    fh=open(f'{doc_name}.txt', 'w')
-    fh.write(output)
-    fh.close()
+            doc_name = f'{site["hostname"]} xper'
+            fh=open(f'{doc_name}.txt', 'w')
+            fh.write(output)
+            fh.close()
+    
+    else:
+        print (f'skipping {site["hostname"]} xper configuration')
+
+
+
 
 def get_xaxs_config():
-    output2 = template2.render(
-    username=site["username"],
-    password=site["password"],
-    hostname=site["hostname"], 
-    third_oct=site["third_octet"])
+    if site["get_xaxs_config"]=="True":
+        output2 = template2.render(
+        username=site["username"],
+        password=site["password"],
+        hostname=site["hostname"], 
+        third_oct=site["third_octet"])
 
-    doc_name = f'{site["hostname"]} xaxs'
-    fh=open(f'{doc_name}.txt', 'w')
-    fh.write(output2)
-    fh.close()
+        doc_name = f'{site["hostname"]} xaxs'
+        fh=open(f'{doc_name}.txt', 'w')
+        fh.write(output2)
+        fh.close()
+
+    else:
+        print (f'skipping {site["hostname"]} xaxs configuration')
+
+    
 
 def get_pcr_config():
-    output3 = template3.render(
-    hostname=site["hostname"], 
-    third_oct=site["third_octet"])
+    if site["get_pcr_config"]=="True":
+        output3 = template3.render(
+        hostname=site["hostname"], 
+        third_oct=site["third_octet"])
 
-    doc_name = f'{site["hostname"]} PCR Configuration'
-    fh=open(f'{doc_name}.txt', 'w')
-    fh.write(output3)
-    fh.close()
+        doc_name = f'{site["hostname"]} PCR Configuration'
+        fh=open(f'{doc_name}.txt', 'w')
+        fh.write(output3)
+        fh.close()
+
+    else:
+        print (f'skipping {site["hostname"]} pcr configuration')
 
 def get_snmpc_config():
-    output4 = template4.render(
-    hostname=site["hostname"], 
-    third_oct=site["third_octet"])
+    if site["get_snmpc_config"]=="True":
+        output4 = template4.render(
+        hostname=site["hostname"], 
+        third_oct=site["third_octet"])
     
-    doc_name = f'{site["hostname"]} SNMPc Configuration'
-    fh=open(f'{doc_name}.txt', 'w')
-    fh.write(output4)
-    fh.close()
+        doc_name = f'{site["hostname"]} SNMPc Configuration'
+        fh=open(f'{doc_name}.txt', 'w')
+        fh.write(output4)
+        fh.close()
+
+    else:
+        print (f'skipping {site["hostname"]} snmpc configuration')
 
 #implement For loop to generate multiple configurations
 for site in Sites:
