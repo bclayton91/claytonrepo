24fc8ecbdbb56b64bc27dd1f258f697ea8f0d126
bclayton7043 <brandonclayton1991gmail.com>
2021-04-28 00:45:54 -0400

    updated error/exception handling

--- Python Programs/Config Automation/vpnRtrGui.py
+++ Python Programs/Config Automation/vpnRtrGui.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from jinja2 import Environment, FileSystemLoader
+import os
 
 def closeprogram():
     window.destroy()
@@ -17,11 +18,19 @@ def get_vpnrtr_config():
     )
 
     docname=(f"{Site}-VPN-RTR-CONFIG")
-    fh=open(f'C:\GitExample1\Git\Python Programs\Config Automation\Configurations\{docname}.txt', 'w')
-    fh.write(output)
-    fh.close()
-    displaytext=f"Configuration generated for site {Site}\n Configuration stored in the following directory: C:\GitExample1\Git\Python Programs\Config Automation\Configurations"
-    outputBox.insert(END,displaytext)
+    try:
+        fh=open(f'C:\GitExample1\Git\Python Programs\Config Automation\Configurations\{docname}.txt', 'w')
+        fh.write(output)
+        fh.close()
+        displaytext=f"Configuration generated for site: \"{Site}\" \n Configuration stored in the following directory: C:\GitExample1\Git\Python Programs\Config Automation\Configurations"
+        outputBox.insert(END,displaytext)
+    except:
+        os.makedirs('C:\GitExample1\Git\Python Programs\Config Automation\Configurations')
+        fh=open(f'C:\GitExample1\Git\Python Programs\Config Automation\Configurations\{docname}.txt', 'w')
+        fh.write(output)
+        fh.close()
+        displaytext=f"Configuration generated for site {Site}\n Configuration stored in the following directory: C:\GitExample1\Git\Python Programs\Config Automation\Configurations"
+        outputBox.insert(END,displaytext)
 
 file_loader=FileSystemLoader('C:\GitExample1\Git\Python Programs\Config Automation\Templates')
 env=Environment(loader=file_loader)
@@ -37,37 +46,37 @@ photoLabel=Label (window, image=photo1, bg="deep sky blue")
 photoLabel.grid(row=0, column=0, sticky=W)
 
 siteNameLabel=Label(window, text="Enter Site Name:", bg="black", fg="white", width=15,
-font="none 11 bold")
+font="none 11 bold", anchor=W)
 siteNameLabel.grid(row=1, column=0, sticky=W)
 siteNameTextBox=Entry(window, width=23, bg="white")
 siteNameTextBox.grid(row=2, column=0, pady=(0,20), sticky=W)
 
 siteIdLabel=Label(window, text="Enter Site Id", bg="black", fg="white", width=15,
-font="none 11 bold")
+font="none 11 bold", anchor=W)
 siteIdLabel.grid(row=3, column=0, sticky=W)
 siteIdTextBox=Entry(window, width=23, bg="white")
 siteIdTextBox.grid(row=4, column=0, sticky=W, pady=(0,20))
 
 primaryHubLabel=Label(window, text="Enter primary hub ip address", bg="black", fg="white",
-font="none 11 bold", width=25)
+font="none 11 bold", width=25, anchor=W)
 primaryHubLabel.grid(row=5, column=0, sticky=W)
 primaryHubTextBox=Entry(window, width=38, bg="white")
 primaryHubTextBox.grid(row=6, column=0, sticky=W, pady=(0,20))
 
 secondaryHubLabel=Label(window, text="Enter secondary hub ip address", bg="black", fg="white",
-font="none 11 bold", width=25)
+font="none 11 bold", width=25, anchor=W)
 secondaryHubLabel.grid(row=7, column=0, sticky=W)
 secondaryHubTextBox=Entry(window, bg="white", width=38)
 secondaryHubTextBox.grid(row=8, column=0, sticky=W, pady=(0,20))
 
 tertiaryHubLabel=Label(window, text="Enter tertiary hub ip address", bg="black", fg="white",
-font="none 11 bold", width=25)
+font="none 11 bold", width=25, anchor=W)
 tertiaryHubLabel.grid(row=9, column=0, sticky=W)
 tertiaryHubTextBox=Entry(window, bg="white", width=38)
 tertiaryHubTextBox.grid(row=10, column=0, sticky=W, pady=(0,20))
 
 communityStringLabel=Label(window, text="Enter community string", bg="Black", fg="white",
-font="none 11 bold", width=25)
+font="none 11 bold", width=25, anchor=W)
 communityStringLabel.grid(row=11, column=0, sticky=W)
 communityStringTextBox=Entry(window, bg="white", width=38)
 communityStringTextBox.grid(row =12, column=0, pady=(0,20), sticky=W)
